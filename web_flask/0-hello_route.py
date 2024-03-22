b2bd862d0a64265c41e7950c2bb98efdbca003fd
Thige254 <thigetj@gmail.com>
2024-03-22 16:48:11 +0000

    server 0x1A-application_server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
-
-
+""" This module starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    """ This function returns a greeting """
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
